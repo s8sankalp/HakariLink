b4953b19c30265802d1ed7328eca3219df5b7aa7
sankalp suman <sankalpsuman08@gmail.com>
2026-04-03 23:33:15 +0530

    jwt auth

---
+++ shorter/src/main/java/com/url/shorter/security/jwt/JwtUtils.java
@@ -0,0 +1,69 @@
+package com.url.shorter.security.jwt;
+
+import com.url.shorter.services.UserDetailImpl;
+import io.jsonwebtoken.JwtBuilder;
+import io.jsonwebtoken.JwtException;
+import io.jsonwebtoken.Jwts;
+import io.jsonwebtoken.security.Keys;
+import jakarta.servlet.http.HttpServletRequest;
+import org.springframework.beans.factory.annotation.Value;
+import tools.jackson.databind.node.StringNode;
+
+import javax.crypto.SecretKey;
+import java.util.Date;
+import java.util.stream.Collectors;
+
+public class JwtUtils {
+    @Value("${jwt.secret}")
+    private String jwtSecret;
+    @Value("${jwt.expiration}")
+    private int jwtExpiration;
+    public String getJwtFromHeader(HttpServletRequest request)
+    {
+
+        //Authorization-> Bearer <TOKEN>
+        String bearerToken=request.getHeader("Authorization");
+        if(bearerToken!=null&& bearerToken.startsWith("Bearer "))
+        {
+            return bearerToken.substring(7);
+        }
+        return null;
+    }
+    public String generateToken(UserDetailImpl userDetails)
+    {
+        String username=userDetails.getUsername();
+        String roles=userDetails.getAuthorities().stream().map(authority ->authority.getAuthority())
+                .collect(Collectors.joining(","));
+        return Jwts.builder()
+                .subject(username)
+                .claim("roles",roles)
+                .issuedAt(new Date())
+                .expiration(new Date(new Date().getTime()+jwtExpiration))
+                .signWith(key())
+                .compact();
+    }
+    public String getUserNameFromJwtToken(String token)
+    {
+        return Jwts.parser()
+                .verifyWith((SecretKey) key())
+                .build().parseSignedClaims(token)
+                .getPayload().getSubject();
+    }
+    private Key key(){
+        return Keys.hmacShaKeyFor(Descoders.BASE64.decode(jwtSecret));
+    }
+    public boolean validateToken(String authToken)
+    {
+        try {
+            Jwts.parser().verifyWith((SecretKey) key())
+                    .build().parseSignedClaims(authToken);
+            return true;
+        } catch (JwtException e) {
+            throw new RuntimeException(e);
+        } catch (IllegalArgumentException e) {
+            throw new RuntimeException(e);
+        }
+        catch (Exception e) {
+            throw new RuntimeException(e);
+    }
+}
---
+++ shorter/src/main/java/com/url/shorter/security/jwt/jwtAuthenticationResponse.java
@@ -0,0 +1,8 @@
+package com.url.shorter.security.jwt;
+
+import lombok.Data;
+
+@Data
+public class jwtAuthenticationResponse {
+    private String token;
+}
