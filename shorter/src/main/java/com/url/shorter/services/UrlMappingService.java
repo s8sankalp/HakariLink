b7787b4f72a28c45807aaee2cc7cc94ce6779544
sankalp suman <sankalpsuman08@gmail.com>
2026-04-11 00:03:32 +0530

    Add getOriginalUrl method with click event tracking in UrlMappingService

--- shorter/src/main/java/com/url/shorter/services/UrlMappingService.java
+++ shorter/src/main/java/com/url/shorter/services/UrlMappingService.java
@@ -88,4 +88,19 @@ String shortURl=generateShortUrl();
         return clickEvents.stream()
                 .collect(Collectors.groupingBy(click->click.getClickDate().toLocalDate(),Collectors.counting()));
     }
+
+    public UrlMapping getOriginalUrl(String shortUrl) {
+        UrlMapping urlMapping=urlMappingRepository.findByShortUrl(shortUrl);
+        if(urlMapping!=null)
+        {
+            urlMapping.setClickCount(urlMapping.getClickCount()+1);
+            urlMappingRepository.save(urlMapping);
+            ClickEvent clickEvent=new ClickEvent();
+            clickEvent.setClickDate(LocalDateTime.now());
+            clickEvent.setUrlMapping(urlMapping);
+            clickEventRepository.save(clickEvent);
+
+        }
+        return urlMapping;
+    }
 }
